38eb3c7a5b86f66995e287bd5d40dfee8d892936
Scott Woods <scott@westarete.com>
2014-04-04 19:04:23 -0400

    Install and load forecast gem

--- app.rb
+++ app.rb
@@ -4,6 +4,10 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'geocoder'
 require 'multi_json'
+require 'forecast_io'
+
+# Set our API key for talking to forecast.io
+ForecastIO.api_key = 'dc9060a06370dd03b46af35827653a8c'
 
 # Make sure we're using a gem that works on both Mac and Windows.
 MultiJson.use(:json_pure)
