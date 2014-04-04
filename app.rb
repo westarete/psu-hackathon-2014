a4e372c0228d779ab849ba70f705d3ab63484fbd
Scott Woods <scott@westarete.com>
2014-04-04 18:48:58 -0400

    Fix missing gem

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'geocoder'
+require 'multi_json'
 
 # Make sure we're using a gem that works on both Mac and Windows.
 MultiJson.use(:json_pure)
