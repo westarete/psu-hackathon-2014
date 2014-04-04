6e717f2da566e32a41ddcd759e1b0156e1828a75
Scott Woods <scott@westarete.com>
2014-04-04 18:40:21 -0400

    Geocode our first location
    Hard coded.

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'geocoder'
+
+# Make sure we're using a gem that works on both Mac and Windows.
+MultiJson.use(:json_pure)
 
 get '/' do
+  @location = Geocoder.search("penn state").first
   erb :index
 end
